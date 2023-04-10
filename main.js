3e5ad6a5758604273ec239dbcf039535437136fe
GeoLine02 <126957977+GeoLine02@users.noreply.github.com>
2023-04-10 18:18:35 +0400

    project is  finished

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const hamburger = document.querySelector(".hamburger");
 const u_list = document.querySelector(".u_list");
-const emileInput = document.getElementById("emileInput");
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   u_list.classList.toggle("active");
